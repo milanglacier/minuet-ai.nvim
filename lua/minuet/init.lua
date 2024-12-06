ea06ce3c9631ceed2eed079fa3cbffdc4dd376f7
Milan Glacier <dev@milanglacier.com>
2024-12-06 18:16:12 -0500

    refactor: rename source.lua to cmp.lua
    Rename minuet/source.lua to minuet/cmp.lua for better clarity.

--- lua/minuet/init.lua
+++ lua/minuet/init.lua
@@ -4,7 +4,7 @@ local M = {}
 
 function M.setup(config)
     M.config = vim.tbl_deep_extend('force', default_config, config or {})
-    require('cmp').register_source('minuet', require('minuet.source'):new())
+    require('cmp').register_source('minuet', require('minuet.cmp'):new())
 end
 
 function M.make_cmp_map()
