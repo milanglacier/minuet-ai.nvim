63dec03665d1efcf1f87ecac45d39895ca26af96
Milan Glacier <dev@milanglacier.com>
2024-12-16 13:27:22 -0500

    Revert "fix: make_context_filter_sequence should removes leading whitespaces in each line."
    This reverts commit fa15e086343233b022ebe6c77e33354946b591e6.

--- lua/minuet/utils.lua
+++ lua/minuet/utils.lua
@@ -192,8 +192,6 @@ function M.make_context_filter_sequence(context, length)
 
     -- remove leading whitespaces
     context = context:gsub('^%s+', '')
-    -- remove leading whitespaces in each line
-    context = context:gsub('\n%s+', '\n')
 
     if vim.fn.strchars(context) < length then
         return
