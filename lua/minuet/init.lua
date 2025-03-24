4c0d1bdb6b3580af9aa11479447c584613094cf9
Abizrh <abizarah857@gmail.com>
2025-03-24 17:05:30 +0700

    🐛 fix: correct typo in preset name

--- lua/minuet/init.lua
+++ lua/minuet/init.lua
@@ -4,7 +4,7 @@ local M = {}
 
 function M.setup(config)
     M.presets = config.presets or {}
-    M.presets.orignal = config
+    M.presets.original = config
 
     config.presets = nil
     M.config = vim.tbl_deep_extend('force', default_config, config or {})
