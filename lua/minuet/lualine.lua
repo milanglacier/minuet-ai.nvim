12e1f891b13046cae5151bed349a0b7a2d62f15a
Jari Meevis <jarimeevis@gmail.com>
2025-04-20 01:43:00 +0200

    feat(lualine): make configurable

--- lua/minuet/lualine.lua
+++ lua/minuet/lualine.lua
@@ -4,26 +4,34 @@ M.processing = false
 M.spinner_index = 1
 M.n_requests = 1
 M.n_finished_requests = 0
-M.name = nil
+M.provider = nil
 M.model = nil
 
-local spinner_symbols = {
-    '⠋',
-    '⠙',
-    '⠹',
-    '⠸',
-    '⠼',
-    '⠴',
-    '⠦',
-    '⠧',
-    '⠇',
-    '⠏',
+local default_options = {
+    -- the symbols that are used to create spinner animation
+    spinner_symbols = {
+        '⠋',
+        '⠙',
+        '⠹',
+        '⠸',
+        '⠼',
+        '⠴',
+        '⠦',
+        '⠧',
+        '⠇',
+        '⠏',
+    },
+    -- the name displayed in the lualine. Set to "provider", "model" or "both"
+    display_name = 'both',
+    -- separator between provider and model name for option "both"
+    provider_model_separator = ':',
 }
-local spinner_symbols_len = #spinner_symbols
 
 -- Initializer
 function M:init(options)
     M.super.init(self, options)
+    self.options = vim.tbl_extend('keep', self.options or {}, default_options)
+    self.spinner_symbols_len = #self.options.spinner_symbols
 
     local group = vim.api.nvim_create_augroup('MinuetLualine', { clear = true })
 
@@ -35,8 +43,15 @@ function M:init(options)
             self.processing = false
             self.n_requests = data.n_requests
             self.n_finished_requests = 0
-            self.name = data.name
+            self.provider = data.name
             self.model = data.model
+            if self.options.display_name == 'model' then
+                self.display_name = self.model
+            elseif self.options.display_name == 'provider' then
+                self.display_name = self.provider
+            else
+                self.display_name = self.provider .. self.options.provider_model_separator .. self.model
+            end
         end,
     })
 
@@ -63,14 +78,11 @@ end
 -- Function that runs every time statusline is updated
 function M:update_status()
     if self.processing then
-        self.spinner_index = (self.spinner_index % spinner_symbols_len) + 1
-        local request =
-            string.format('%s/%s: %s/%s', self.name, self.model, self.n_finished_requests + 1, self.n_requests)
-        return request .. ' ' .. spinner_symbols[self.spinner_index]
+        self.spinner_index = (self.spinner_index % self.spinner_symbols_len) + 1
+        local request = string.format('%s: %s/%s', self.display_name, self.n_finished_requests + 1, self.n_requests)
+        return request .. ' ' .. self.options.spinner_symbols[self.spinner_index]
     else
-        if self.name ~= nil and self.model ~= nil then
-            return self.name .. '/' .. self.model
-        end
+        return self.display_name
     end
 end
 
