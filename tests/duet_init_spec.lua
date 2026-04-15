fec9cd9f764f1165b260d4bf7e71cf3506189820
Milan Glacier <dev@milanglacier.com>
2026-04-14 22:17:56 -0400

    chore: add more tests for `minuet.duet`.

--- tests/duet_init_spec.lua
+++ tests/duet_init_spec.lua
@@ -4,7 +4,7 @@ return {
     {
         name = 'duet.action.predict followed by apply updates the buffer and cursor',
         run = function()
-            helpers.setup_root_config({
+            helpers.setup_root_config {
                 duet = {
                     provider = 'test',
                     editable_region = {
@@ -15,7 +15,7 @@ return {
                         cursor = '|',
                     },
                 },
-            })
+            }
 
             local pending_callback
             local seen_context
@@ -57,7 +57,7 @@ return 42<cursor_position>
     {
         name = 'duet.action.predict discards stale provider responses',
         run = function()
-            helpers.setup_root_config({
+            helpers.setup_root_config {
                 duet = {
                     provider = 'test',
                     editable_region = {
@@ -65,7 +65,7 @@ return 42<cursor_position>
                         lines_after = 0,
                     },
                 },
-            })
+            }
 
             local pending_callback
 
@@ -81,6 +81,7 @@ return 42<cursor_position>
             local bufnr = helpers.create_buffer({ 'return 1' }, { 1, 8 })
 
             duet.action.predict()
+            helpers.expect_truthy(pending_callback, 'backend callback was not captured')
             vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { 'return 7' })
 
             pending_callback [[<editable_region_start>
@@ -93,6 +94,104 @@ return 42<cursor_position>
 
             helpers.expect_equal(vim.api.nvim_buf_get_lines(bufnr, 0, -1, false), { 'return 7' })
             helpers.expect_falsy(duet.action.is_visible(), 'stale duet preview should not render')
+
+            helpers.delete_buffer(bufnr)
+        end,
+    },
+    {
+        name = 'duet.action.dismiss clears the preview without changing the buffer',
+        run = function()
+            helpers.setup_root_config {
+                duet = {
+                    provider = 'test',
+                    editable_region = {
+                        lines_before = 0,
+                        lines_after = 0,
+                    },
+                },
+            }
+
+            local pending_callback
+
+            package.loaded['minuet.duet.backends.test'] = {
+                complete = function(_, callback)
+                    pending_callback = callback
+                end,
+            }
+
+            local duet = helpers.reload 'minuet.duet'
+            duet.setup()
+
+            local bufnr = helpers.create_buffer({ 'return 1' }, { 1, 8 })
+
+            duet.action.predict()
+            helpers.expect_truthy(pending_callback, 'backend callback was not captured')
+
+            pending_callback [[<editable_region_start>
+return 42<cursor_position>
+<editable_region_end>]]
+
+            helpers.wait_until(function()
+                return duet.action.is_visible()
+            end, 1000, 'duet preview did not become visible')
+
+            duet.action.dismiss()
+
+            helpers.expect_falsy(duet.action.is_visible(), 'preview should be cleared after dismiss')
+            helpers.expect_equal(vim.api.nvim_buf_get_lines(bufnr, 0, -1, false), { 'return 1' })
+
+            duet.action.apply()
+
+            helpers.expect_equal(vim.api.nvim_buf_get_lines(bufnr, 0, -1, false), { 'return 1' })
+
+            helpers.delete_buffer(bufnr)
+        end,
+    },
+    {
+        name = 'duet.action.apply becomes a no-op after the preview is cleared by editing',
+        run = function()
+            helpers.setup_root_config {
+                duet = {
+                    provider = 'test',
+                    editable_region = {
+                        lines_before = 0,
+                        lines_after = 0,
+                    },
+                },
+            }
+
+            local pending_callback
+
+            package.loaded['minuet.duet.backends.test'] = {
+                complete = function(_, callback)
+                    pending_callback = callback
+                end,
+            }
+
+            local duet = helpers.reload 'minuet.duet'
+            duet.setup()
+
+            local bufnr = helpers.create_buffer({ 'return 1' }, { 1, 8 })
+
+            duet.action.predict()
+            helpers.expect_truthy(pending_callback, 'backend callback was not captured')
+
+            pending_callback [[<editable_region_start>
+return 42<cursor_position>
+<editable_region_end>]]
+
+            helpers.wait_until(function()
+                return duet.action.is_visible()
+            end, 1000, 'duet preview did not become visible')
+
+            vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { 'return 7' })
+            vim.api.nvim_exec_autocmds('TextChanged', { buffer = bufnr, modeline = false })
+
+            helpers.expect_falsy(duet.action.is_visible(), 'preview should be cleared after editing the buffer')
+
+            duet.action.apply()
+
+            helpers.expect_equal(vim.api.nvim_buf_get_lines(bufnr, 0, -1, false), { 'return 7' })
 
             helpers.delete_buffer(bufnr)
         end,
